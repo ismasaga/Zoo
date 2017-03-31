2cd29f29a5b3dadad842bf13e61f7361590810b8
miguel <miguel@miguel-Inspiron-5559>
2017-03-31 09:30:47 +0200

    prueba2

--- src/baseDatos/DAOAbstracto.java
+++ src/baseDatos/DAOAbstracto.java
@@ -13,5 +13,21 @@ public abstract class DAOAbstracto {
 
     private aplicacion.FachadaAplicacion fa;
     private java.sql.Connection conexion;
+
+    protected java.sql.Connection getConexion(){
+        return this.conexion;
+    }
+
+    protected void setConexion(java.sql.Connection conexion){
+        this.conexion=conexion;
+    }
+
+    protected void setFachadaAplicacion(aplicacion.FachadaAplicacion fa){
+        this.fa=fa;
+    }
+
+    protected aplicacion.FachadaAplicacion getFachadaAplicacion(){
+        return fa;
+    }
     
 }
