a80a53fbbfd180c1f8cf0cd8fc67ccb5b4bb2ebf
ismasaga <ismael.saborido@rai.usc.es>
2017-04-08 17:09:29 +0200

    Preparado para seguir

--- src/aplicacion/Controller/CoidadorController.java
+++ src/aplicacion/Controller/CoidadorController.java
@@ -3,6 +3,130 @@ package aplicacion.Controller;
 import aplicacion.Animal;
 import aplicacion.Usuario;
 import gui.GUIManager;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CoidadorController implements Initializable {
+    ObservableList<Animal> animales = FXCollections.observableArrayList();
+    @FXML
+    private Button sesionButton;
+    @FXML
+    private Button sairButton;
+    @FXML
+    private Button buscarButton;
+    @FXML
+    private TextField buscarTextField;
+    @FXML
+    private Button todosButton;
+    @FXML
+    private VBox tarefasVBox;
+    @FXML
+    private Pane panelAnimaisTabla;
+    @FXML
+    private TableView tabla;
+    private TableColumn<Animal, String> first = new TableColumn<Animal, String>("ID");
+    private TableColumn<Animal, String> second = new TableColumn<Animal, String>("Nombre");
+    private TableColumn<Animal, String> third = new TableColumn<Animal, String>("Especie");
+    private TableColumn<Animal, Integer> fourth = new TableColumn<Animal, Integer>("Edad");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void initUser(final GUIManager GUIManager, Usuario usuario) throws IOException {
+        //sessionLabel.setText(sessionID);
+        sesionButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                GUIManager.logout();
+
+            }
+        });
+        sairButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                GUIManager.sair();
+
+            }
+        });
+
+        buscarButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    tabla = (FXMLLoader.load(getClass().getResource("/gui/FXML/TaboaAnimais.fxml")));
+                    panelAnimaisTabla.getChildren().add(tabla);
+                    String animal = buscarTextField.getText();
+                    animales = GUIManager.buscarAnimal(animal);
+                    tabla.setItems(animales);
+                    first.setCellValueFactory(cellData -> cellData.getValue().idProperty());
+                    second.setCellValueFactory(cellData -> cellData.getValue().nombreProperty());
+                    third.setCellValueFactory(cellData -> cellData.getValue().especieProperty());
+                    tabla.getColumns().add(first);
+                    tabla.getColumns().add(second);
+                    tabla.getColumns().add(third);
+                    tabla.getSelectionModel().select(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        buscarTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode().equals(KeyCode.ENTER)) {
+                    buscarButton.fire();
+                }
+            }
+        });
+
+        todosButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                buscarTextField.setText("");
+                buscarButton.fire();
+            }
+        });
+
+        buscarButton.fire();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+/******************************************************************************/
+
+/*package aplicacion.Controller;
+
+import aplicacion.Animal;
+import aplicacion.Usuario;
+import gui.GUIManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -44,11 +168,11 @@ public class CoidadorController {
     private GUIManager GUIManager;
 
     @FXML
-    private void initialize() {
+    private void initialize() {*/
         /* Dende aqui non podo facer nada porque non tenho maneira de facer
          * consultas nin de chamar a ningen
          */
-    }
+    /*}
 
     public void initUser(final GUIManager GUIManager, Usuario usuario) throws IOException {
         // Gardo a referencia o GUIManager para poder chamalo dende calquer metodo
@@ -74,7 +198,7 @@ public class CoidadorController {
             }
         });
 
-        /*buscarButton.setOnAction(new EventHandler<ActionEvent>() {
+        buscarButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 try {
@@ -94,9 +218,9 @@ public class CoidadorController {
                     e.printStackTrace();
                 }
             }
-        });*/
+        });
 
-        /* buscarTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        buscarTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 if (event.getCode().equals(KeyCode.ENTER)) {
@@ -111,15 +235,15 @@ public class CoidadorController {
                 buscarTextField.setText("");
                 buscarButton.fire();
             }
-        });*/
-        //buscarButton.fire();
+        });
+        buscarButton.fire();
     }
 
     @FXML
     private void buscarAnimais() {
         String animal = buscarTextField.getText();
         tabla.setItems(GUIManager.buscarAnimal(animal));
-        /*try {
+        try {
             tabla = (FXMLLoader.load(getClass().getResource("/gui/FXML/TaboaAnimais.fxml")));
             panelAnimaisTabla.getChildren().add(tabla);
             String animal = buscarTextField.getText();
@@ -134,6 +258,6 @@ public class CoidadorController {
             tabla.getSelectionModel().select(0);
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
-}
+}*/
